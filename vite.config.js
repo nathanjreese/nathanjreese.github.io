8590fb64ec1eb3a31af044b4df9a2ff5fb141697
Nathan <nathanjreese@gmail.com>
2024-02-09 23:43:20 -0500

    change config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/indycardriverapp',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
