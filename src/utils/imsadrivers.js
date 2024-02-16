9807755d51d90e21ead852172ea50ddbe99d49ae
Nathan <nathanjreese@gmail.com>
2024-02-15 19:20:07 -0500

    commit

--- src/utils/imsadrivers.js
+++ src/utils/imsadrivers.js
@@ -7,7 +7,7 @@ async function getAllDrivers(){
         password: import.meta.env.VITE_DATABASE_PASSWORD
       }
       const conn = connect(config)
-      const results = await conn.execute('Select * from imsa_timing ORDER BY overall_position')
+      // const results = await conn.execute('Select * from imsa_timing ORDER BY overall_position')
       return(results.rows)
  }
 
