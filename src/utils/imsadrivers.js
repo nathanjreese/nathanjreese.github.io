c70d033957a099aaa0d36a670a4bd202531b025b
Nathan <nathanjreese@gmail.com>
2024-02-18 20:16:25 -0500

    imsa

--- src/utils/imsadrivers.js
+++ src/utils/imsadrivers.js
@@ -4,7 +4,7 @@ async function getAllDrivers(){
     const config = {
         host: import.meta.env.VITE_DATABASE_URL,
         username: import.meta.env.VITE_DATABASE_USER,
-        password: import.meta.env.VITE_DATABASE_PASSWORD
+        password: import.meta.env.VITE_DATABASE_ONE + import.meta.env.VITE_DATABASE_TWO
       }
       const conn = connect(config)
       const results = await conn.execute('Select * from imsa_timing ORDER BY overall_position')
