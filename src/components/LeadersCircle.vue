65f2c7cfebb89eac9b9f42f5a23e9ad857f8fe1e
Nathan <nathanjreese@gmail.com>
2024-04-22 20:46:26 -0400

    update leaders circle

--- src/components/LeadersCircle.vue
+++ src/components/LeadersCircle.vue
@@ -51,31 +51,31 @@ import TitlePage from '@/components/Partials/Title'
     data () {
       return {
         standings: [
-        {team:"Team Penske",number:2,points:0,races:{stpete:54,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Team Penske",number:3,points:0,races:{stpete:35,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Arrow McLaren",number:5,points:0,races:{stpete:40,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Arrow McLaren",number:6,points:0,races:{stpete:17,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Arrow McLaren",number:7,points:0,races:{stpete:24,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Chip Ganassi Racing",number:8,points:0,races:{stpete:7,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Chip Ganassi Racing",number:9,points:0,races:{stpete:22,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Chip Ganassi Racing",number:10,points:0,races:{stpete:28,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Team Penske",number:12,points:0,races:{stpete:32,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"A.J. Foyt Racing",number:14,points:0,races:{stpete:19,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Rahal Letterman Lanigan",number:15,points:0,races:{stpete:14,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Dale Coyne Racing",number:18,points:0,races:{stpete:11,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Ed Carpenter Racing",number:20,points:0,races:{stpete:9,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Ed Carpenter Racing",number:21,points:0,races:{stpete:20,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Andretti Global",number:26,points:0,races:{stpete:31,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Andretti Global",number:27,points:0,races:{stpete:18,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Andretti Global",number:28,points:0,races:{stpete:5,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Rahal Letterman Lanigan",number:30,points:0,races:{stpete:15,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"A.J. Foyt Racing",number:41,points:0,races:{stpete:5,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Rahal Letterman Lanigan",number:45,points:0,races:{stpete:11,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Dale Coyne Racing",number:51,points:0,races:{stpete:8,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Meyer Shank Racing",number:60,points:0,races:{stpete:26,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Meyer Shank Racing",number:66,points:0,races:{stpete:13,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Juncos Hollinger Racing",number:77,points:0,races:{stpete:6,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
-          {team:"Juncos Hollinger Racing",number:78,points:0,races:{stpete:12,longbeach:0,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}}          
+        {team:"Team Penske",number:2,points:0,races:{stpete:54,longbeach:33,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Team Penske",number:3,points:0,races:{stpete:35,longbeach:5,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Arrow McLaren",number:5,points:0,races:{stpete:40,longbeach:14,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Arrow McLaren",number:6,points:0,races:{stpete:17,longbeach:19,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Arrow McLaren",number:7,points:0,races:{stpete:24,longbeach:20,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Chip Ganassi Racing",number:8,points:0,races:{stpete:7,longbeach:17,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Chip Ganassi Racing",number:9,points:0,races:{stpete:22,longbeach:53,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Chip Ganassi Racing",number:10,points:0,races:{stpete:28,longbeach:35,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Team Penske",number:12,points:0,races:{stpete:32,longbeach:29,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"A.J. Foyt Racing",number:14,points:0,races:{stpete:19,longbeach:9,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Rahal Letterman Lanigan",number:15,points:0,races:{stpete:14,longbeach:13,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Dale Coyne Racing",number:18,points:0,races:{stpete:11,longbeach:5,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Ed Carpenter Racing",number:20,points:0,races:{stpete:9,longbeach:5,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Ed Carpenter Racing",number:21,points:0,races:{stpete:20,longbeach:16,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Andretti Global",number:26,points:0,races:{stpete:31,longbeach:41,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Andretti Global",number:27,points:0,races:{stpete:18,longbeach:27,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Andretti Global",number:28,points:0,races:{stpete:5,longbeach:30,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Rahal Letterman Lanigan",number:30,points:0,races:{stpete:15,longbeach:6,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"A.J. Foyt Racing",number:41,points:0,races:{stpete:5,longbeach:12,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Rahal Letterman Lanigan",number:45,points:0,races:{stpete:11,longbeach:7,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Dale Coyne Racing",number:51,points:0,races:{stpete:8,longbeach:10,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Meyer Shank Racing",number:60,points:0,races:{stpete:26,longbeach:24,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Meyer Shank Racing",number:66,points:0,races:{stpete:13,longbeach:8,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Juncos Hollinger Racing",number:77,points:0,races:{stpete:6,longbeach:24,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}},
+          {team:"Juncos Hollinger Racing",number:78,points:0,races:{stpete:12,longbeach:15,barber:0,indygp:0,indy500:0,detroit:0,roadamerica:0,laguna:0,midohio:0,iowa1:0,iowa2:0,toronto:0,gateway:0,portland:0,milwaukee1:0,milwaukee2:0,nashville:0}}          
         ]
       }
     },
@@ -121,7 +121,7 @@ import TitlePage from '@/components/Partials/Title'
 .leaders-table{
   max-width: 900px;
   height: 80%;
-  font-size: calc(8px + .8vw);
+  font-size: calc(10px + .5vw);
   float: center;
   margin-left: auto;
   margin-right: auto;
@@ -133,6 +133,7 @@ import TitlePage from '@/components/Partials/Title'
     color: whitesmoke;
     background-color: #323232;
   }
+  td{padding: 4px;}
 }
 tr:nth-child(even)
 {
