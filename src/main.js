07f7110e49274097d42d9e674a15efd0962d7d66
Nathan <nathanjreese@gmail.com>
2026-03-29 12:53:19 -0400

    push main updates

--- src/main.js
+++ src/main.js
@@ -19,11 +19,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faCartShopping, faUserSecret, faXmark, faFlag, faCaretRight, faIdCard, faRetweet, faCircleQuestion, faPlus, faListCheck, faCircleChevronLeft, faCircleChevronRight, faCircleInfo} from '@fortawesome/free-solid-svg-icons'
-import { faFacebook, faInstagram, faTwitter, faTwitch, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons'
+import { faCartShopping, faUserSecret, faXmark, faFlag, faCaretRight, faIdCard, faRetweet, faCircleQuestion, faPlus, faListCheck, faCircleChevronLeft, faCircleChevronRight, faCircleInfo, faCamera, faShareNodes} from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faTwitter, faTwitch, faYoutube, faTiktok, faXTwitter, faBluesky, faThreads } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret, faListCheck, faXmark, faFlag, faCaretRight, faRetweet, faCircleQuestion, faTwitter, faTwitch, faYoutube, faTiktok, faFacebook, faInstagram, faCartShopping, faCircleInfo, faIdCard, faPlus, faCircleChevronLeft, faCircleChevronRight)
+library.add(faUserSecret, faListCheck, faXmark, faFlag, faCaretRight, faRetweet, faCircleQuestion, faTwitter, faTwitch, faYoutube, faTiktok, faFacebook, faInstagram, faCartShopping, faCircleInfo, faIdCard, faPlus, faCircleChevronLeft, faCircleChevronRight, faCamera, faShareNodes, faXTwitter, faBluesky, faThreads)
 
 // Composables
 import { createApp } from 'vue'
